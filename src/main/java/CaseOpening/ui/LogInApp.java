965ff179637a499a411dd6b3eb49f9b4ab85b955
madsab <mads.barnes@hotmail.com>
2023-03-21 21:26:11 +0100

    LogInApp complete

--- src/main/java/CaseOpening/ui/LogInApp.java
+++ src/main/java/CaseOpening/ui/LogInApp.java
@@ -1,6 +1,20 @@
-package ui;
+package CaseOpening.ui;
 
-
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
 public class LogInApp extends Application{
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My App");
+        primaryStage.setScene(FXMLLoader.load(LogInApp.class.getResource("loggInPage.fxml")));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        LogInApp.launch(args);
+    }
      
 }
