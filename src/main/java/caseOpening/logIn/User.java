c95c5abfb0bba22814aa8a389301061f8902f0cc
madsab <mads.barnes@hotmail.com>
2023-03-23 14:52:30 +0100

    Completed password and username verifying

--- src/main/java/caseOpening/logIn/User.java
+++ src/main/java/caseOpening/logIn/User.java
@@ -3,12 +3,24 @@ package caseOpening.logIn;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import caseOpening.weapons.Weapons;
 
 public class User {
     private String username, password;
     private int keys;
     private List<Weapons> aqquiredWeapons = new ArrayList<>();
+
+    /*
+     * Constructors
+     */
+    public User(String username, String password){
+        if(username == null || password == null){
+            throw new IllegalArgumentException("Brukernavn/Passord kan ikke være null");
+        }
+        this.setUsername(username);
+        this.setPassword(password);
+    }
 
     /*
      * Getter-methods
@@ -39,6 +51,9 @@ public class User {
     }
 
     public void setUsername(String username) {
+        if(!isValidUsername(username)){
+            throw new IllegalArgumentException("Not valid username. No symbols or digits");
+        }
         this.username = username;
     }
 
@@ -50,9 +65,17 @@ public class User {
      * Private validate methods
      */
     private boolean isValidPassword(String password){
-     if(password.contains("[A-Z0-9]")){
-        return true;
-     }
-     return false;
+        // password contains at least one capital letter and one number
+        return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$");
+        
+    }
+
+    private boolean isValidUsername(String username){
+        //Check if username contains any of this special characters or numbers
+        return !username.matches("^.*['§*!#$%&/()=?+0-9]");
+    }
+
+    public static void main(String[] args) {
+        User user = new User("Jon", "Hallo9");
     }
 }
