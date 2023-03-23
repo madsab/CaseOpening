d1eccd43ad04fca1e712fc5045aefee7573a112b
madsab <mads.barnes@hotmail.com>
2023-03-23 14:52:53 +0100

    Started User.java testing

---
+++ src/test/java/exampleproject/UserTest.java
@@ -0,0 +1,18 @@
+package exampleproject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import caseOpening.logIn.User;
+
+public class UserTest {
+    @Test
+    public void testConstructor(){
+        assertThrows(IllegalArgumentException.class, () -> new User(null, null));
+        User validUser = new User("GeirKL", "Mongo123");
+        
+    }
+}
