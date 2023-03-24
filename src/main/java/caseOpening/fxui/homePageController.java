407393ffb6d20cc0cb51ddf8f58c13ab45389e30
madsab <mads.barnes@hotmail.com>
2023-03-24 22:09:43 +0100

    added to/showShootingRange and to/showCaseOpening

--- src/main/java/caseOpening/fxui/homePageController.java
+++ src/main/java/caseOpening/fxui/homePageController.java
@@ -1,5 +1,27 @@
 package caseOpening.fxui;
 
+import javafx.fxml.FXML;
+
 public class homePageController {
     
+
+    @FXML
+    public void toCaseOpening(){
+
+    }
+
+    @FXML
+    public void toShootingRange(){
+
+    }
+
+    @FXML
+    public void showShootingRange(){
+
+    }
+
+    @FXML
+    public void showCaseOpening(){
+
+    }
 }
