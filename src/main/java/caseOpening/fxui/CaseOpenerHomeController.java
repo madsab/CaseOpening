9915e8fc92b1e38b20687c62d1760fb936c521f5
madsab <mads.barnes@hotmail.com>
2023-03-27 11:31:23 +0200

    added functions CaseOpener and hoverAnimation

---
+++ src/main/java/caseOpening/fxui/CaseOpenerHomeController.java
@@ -0,0 +1,16 @@
+package caseOpening.fxui;
+
+import javafx.fxml.FXML;
+
+public class CaseOpenerHomeController {
+    
+    @FXML
+    public void CaseOpener(){
+
+    }
+
+    @FXML
+    public void hoverAnimation(){
+        
+    }
+}
