e16c30c1b27c07508fc936054512ccffcbd7229b
madsab <mads.barnes@hotmail.com>
2023-03-27 14:59:09 +0200

    added function toCaseOpening for switching pages

--- src/main/java/caseOpening/fxui/homePageController.java
+++ src/main/java/caseOpening/fxui/homePageController.java
@@ -1,10 +1,17 @@
 package caseOpening.fxui;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class homePageController {
     @FXML private Button shootingRangeButton, caseButton;
@@ -13,14 +20,23 @@ public class homePageController {
     private String ShootingRangeInfo = "Test your weapons at the shooting range. \n Nothing better than firing a couple of bullets \n at your enemies, and of course feeling the \n rush of shooting what you own. Be safe";
     private String CaseOpeningInfo = "Place your bets and push your luck in an exiting \n case opening. You can aquire different \n weapons in different rarities. From common \n pistols to the legendary knife. Best of luck";
 
+    //Takes user to CaseOpening main page
     @FXML
-    public void toCaseOpening(){
-        System.out.println("Taper");
+    public void toCaseOpening(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("caseOpening/caseOpenerMainPage.fxml"));
+        Stage stage = ((Stage)((Node)event.getSource()).getScene().getWindow());
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
-
+    //Takes user to ShootingRange
     @FXML
-    public void toShootingRange(){
-        System.out.println("Vinner");
+    public void toShootingRange(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("caseOpening/shootingRange.fxml"));
+        Stage stage = ((Stage)((Node)event.getSource()).getScene().getWindow());
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
@@ -30,7 +46,11 @@ public class homePageController {
         startAssignmentButton.setText("Start shootin");
         startAssignmentButton.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e){
-                toShootingRange();;
+                try {
+                    toShootingRange(e);
+                } catch (IOException e1) {
+                    System.out.println("Feil");
+                }
             }
         });
         homePageInfo.setText(ShootingRangeInfo);
@@ -45,7 +65,11 @@ public class homePageController {
         startAssignmentButton.setText("Open Cases");
         startAssignmentButton.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e){
-                toCaseOpening();
+                try {
+                    toCaseOpening(e);
+                } catch (IOException e1) {
+                    System.out.println("kom hit");
+                }
             }
         });
         homePageInfo.setText(CaseOpeningInfo);
