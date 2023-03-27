d7f22f8015c9bde98bc567f44be0f11c5f1d8a4a
madsab <mads.barnes@hotmail.com>
2023-03-27 14:58:34 +0200

    Added functions OpenCase and toMainPage

--- src/main/java/caseOpening/fxui/CaseOpenerHomeController.java
+++ src/main/java/caseOpening/fxui/CaseOpenerHomeController.java
@@ -1,16 +1,36 @@
 package caseOpening.fxui;
 
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 
 public class CaseOpenerHomeController {
-    
-    @FXML
-    public void CaseOpener(){
 
+
+    @FXML
+    public void OpenCase(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("caseOpening/caseOpener.fxml"));
+        Stage stage = ((Stage)((Node)event.getSource()).getScene().getWindow());
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    public void hoverAnimation(){
-        
+    public void toMainPage(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("caseOpening/homePage.fxml"));
+        Stage stage = ((Stage)((Node)event.getSource()).getScene().getWindow());
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
