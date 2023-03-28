648ff6f3edcf055de96327c034cc96d9cc53b05e
madsab <mads.barnes@hotmail.com>
2023-03-28 19:07:17 +0200

    change of constructor

--- src/main/java/caseOpening/weapons/AutomaticWeapons.java
+++ src/main/java/caseOpening/weapons/AutomaticWeapons.java
@@ -5,14 +5,14 @@ import java.util.concurrent.TimeUnit;
 
 
 public class AutomaticWeapons extends Weapons {
-    public AutomaticWeapons(int fireRatePerSecond, int magazineSize, String camo, String weaponImage) {
-        super(fireRatePerSecond, magazineSize, camo, weaponImage);
+    public AutomaticWeapons(int fireRatePerSecond, int magazineSize, String name, String weaponImage, String rarity) {
+        super(fireRatePerSecond, magazineSize, name, weaponImage, rarity);
     }
     
 
     public void autoFire(){
         while (this.bullets > 0){
-            System.out.println("Fire Bullet " + (magazineSize-bullets));
+            System.out.println("Fire Bullet " + (this.magazineSize-this.bullets));
             try {
                 TimeUnit.MILLISECONDS.sleep(this.fireRate);
             } catch (InterruptedException e) {
