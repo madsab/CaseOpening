2eabd85d80c3eff0d30c840737d1e300365309cd
madsab <mads.barnes@hotmail.com>
2023-03-28 19:07:37 +0200

    change of variable names

--- src/main/java/caseOpening/weapons/Weapons.java
+++ src/main/java/caseOpening/weapons/Weapons.java
@@ -10,25 +10,27 @@ import javafx.scene.image.Image;
 public class Weapons {
     protected int fireRate;
     protected int magazineSize;
-    protected String camo;
+    protected String name;
     protected String weaponImage;
+    protected String rarity;
     protected int bullets;
 
-    public Weapons(int fireRatePerSecond, int magazineSize, String camo, String weaponImage){
+    public Weapons(int fireRatePerSecond, int magazineSize, String name, String weaponImage, String rarity){
         if(fireRatePerSecond <= 0 || magazineSize <= 0){
             throw new IllegalArgumentException("No negative amount");
         }
         this.fireRate = 1000/fireRatePerSecond;
         this.magazineSize = magazineSize;
         this.bullets = magazineSize;
-        this.camo = camo;
+        this.name = name;
         this.weaponImage = weaponImage;
+        this.rarity = rarity;
     }
 
     public void fire(){
-        if(bullets > 0){
-            System.out.println("Fire Bullet " + (magazineSize-bullets));
-            bullets--;
+        if(this.bullets > 0){
+            System.out.println("Fire Bullet " + (this.magazineSize-this.bullets));
+            this.bullets--;
         } else{
             System.out.println("Out of bullets");
         }
@@ -44,20 +46,24 @@ public class Weapons {
      */
     public Image getImage() throws IOException{
         //Gets bytes from image file and makes new image for imageView
-        InputStream stream = new FileInputStream("file:/Users/lottekvalheim/Documents/MADS/Prosjekt_Java/CaseOpening/images/" + weaponImage);
+        InputStream stream = new FileInputStream("file:/Users/lottekvalheim/Documents/MADS/Prosjekt_Java/CaseOpening/images/" + this.weaponImage);
         Image wImage = new Image(stream);
         return wImage;
     }
 
-    public String getCamo() {
-        return camo;
+    public String getName() {
+        return this.name;
     }
 
     public int getFireRate() {
-        return fireRate;
+        return this.fireRate;
     }
 
     public int getMagazineSize() {
-        return magazineSize;
+        return this.magazineSize;
+    }
+
+    public String getRarity() {
+        return rarity;
     }
 }
