477cf8246a117243d6f5ad2b9d7f1fb294550c77
madsab <mads.barnes@hotmail.com>
2023-04-01 20:45:06 +0200

    added new Knife to case

--- src/main/java/caseOpening/openingCases/CaseRegular.java
+++ src/main/java/caseOpening/openingCases/CaseRegular.java
@@ -2,15 +2,16 @@ package caseOpening.openingCases;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
+
 
 import caseOpening.weapons.AutomaticWeapons;
+import caseOpening.weapons.Knife;
 import caseOpening.weapons.Weapons;
 
 public class CaseRegular extends Case {
     private static List<Weapons> availableWeapons = new ArrayList<>(Arrays.asList(
+        new Knife("SwitchBladeGolden", "weapons-SpecialItem.jpg", "legendary"),
         new AutomaticWeapons(10, 30, "AK47Rebel", "weapons-AK47Rebel.jpg", "uncommon"),
         new AutomaticWeapons(13, 30, "AR15Tagged", "weapons-AR15Tagged.jpg", "common"),
         new AutomaticWeapons(11, 35, "SMGDesertDeath", "weapons-SMGDesertDeath.jpg", "rare"),
