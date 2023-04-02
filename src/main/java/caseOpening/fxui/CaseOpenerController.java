9922d27ad264e1e166ab3f3b9c9c44138cb793c5
madsab <mads.barnes@hotmail.com>
2023-04-02 19:50:38 +0200

    setup back button

--- src/main/java/caseOpening/fxui/CaseOpenerController.java
+++ src/main/java/caseOpening/fxui/CaseOpenerController.java
@@ -7,19 +7,25 @@ import java.util.ResourceBundle;
 
 import caseOpening.openingCases.CaseRegular;
 import caseOpening.openingCases.CaseSpinner;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * Controller class for spinning a case
  */
 public class CaseOpenerController implements Initializable {
     private static CaseRegular goldenCase = new CaseRegular();
-    @FXML private ImageView showCaseWeapon1, showCaseWeapon2, showCaseWeapon3, showCaseWeapon4, showCaseWeapon5, caseOpenedBackground, showWeaponWon;
+    @FXML private ImageView showCaseWeapon1, showCaseWeapon2, showCaseWeapon3, showCaseWeapon4, showCaseWeapon5, caseOpenedBackground, showWeaponWon, backIcon;
     @FXML private Pane ShowWeaponPane;
     @FXML private Label weaponName;
 
@@ -35,11 +41,21 @@ public class CaseOpenerController implements Initializable {
         ShowWeaponPane.setVisible(false);
     }
 
+    @FXML
+    public void toCaseOpenerHomePage(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("caseOpening/caseOpenerMainPage.fxml"));
+        Stage stage = ((Stage)((Node)event.getSource()).getScene().getWindow());
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //Set 5 random weapons on loading of scene
         try{
             caseOpenedBackground.setImage(new Image(new FileInputStream("./images/weapons-case-opened.png")));
+            backIcon.setImage(new Image(new FileInputStream("./images/backIcon.png")));
             showCaseWeapon1.setImage(goldenCase.getPrizeWeapon().getImage());
             showCaseWeapon2.setImage(goldenCase.getPrizeWeapon().getImage());
             showCaseWeapon3.setImage(goldenCase.getPrizeWeapon().getImage());
