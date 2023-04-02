4268a2cdf03f5817ba36e68fe254f35786ba7334
madsab <mads.barnes@hotmail.com>
2023-04-02 15:44:10 +0200

    completed spin case and showcase weapon

--- src/main/java/caseOpening/fxui/CaseOpenerController.java
+++ src/main/java/caseOpening/fxui/CaseOpenerController.java
@@ -7,22 +7,33 @@ import java.util.ResourceBundle;
 
 import caseOpening.openingCases.CaseRegular;
 import caseOpening.openingCases.CaseSpinner;
+import caseOpening.weapons.Weapons;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
 /*
  * Class that spins cases
  */
 public class CaseOpenerController implements Initializable {
     private static CaseRegular goldenCase = new CaseRegular();
-    @FXML private ImageView showCaseWeapon1, showCaseWeapon2, showCaseWeapon3, showCaseWeapon4, showCaseWeapon5, caseOpenedBackground;
+    @FXML private ImageView showCaseWeapon1, showCaseWeapon2, showCaseWeapon3, showCaseWeapon4, showCaseWeapon5, caseOpenedBackground, showWeaponWon;
+    @FXML private Pane ShowWeaponPane;
+    @FXML private Label weaponName;
 
     @FXML
     public void spinCase(){
         CaseSpinner caseSpinner = new CaseSpinner(goldenCase,showCaseWeapon1,showCaseWeapon2,showCaseWeapon3,showCaseWeapon4, showCaseWeapon5);
-        caseSpinner.spinCase(10, 0.002);
+        caseSpinner.spinCase(10, 0.002, ShowWeaponPane, showWeaponWon, weaponName);
+        //Gets won weapon and shows it to User
+    }
+
+    @FXML
+    public void hideShowWeapon(){
+        ShowWeaponPane.setVisible(false);
     }
 
     @Override
--- src/main/java/caseOpening/openingCases/CaseSpinner.java
+++ src/main/java/caseOpening/openingCases/CaseSpinner.java
@@ -2,16 +2,23 @@ package caseOpening.openingCases;
 
 
 
+import java.io.IOException;
+
+
 import caseOpening.weapons.Weapons;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 public class CaseSpinner {
     private ImageView[] imageList;
     private Case activeCase;
     private int recursiveStopper = 0;
+    private boolean hasChosenWeapon;
+    private Weapons currentWonWeapon;
     
     public CaseSpinner(Case activCase, ImageView ... weaponImages){
         this.imageList = weaponImages;
@@ -21,26 +28,11 @@ public class CaseSpinner {
     //1060 at end
     //68 at start
     /**
-     *  Spins the case for {@code duration} seconds and stops to give User a prize weapon}
+     *  Spins the case for {@code duration} seconds at {@code speed} per frame. stops to give User a prize weapon}
+     * <p>
+     * Also takes in parameters {@code Pane}, {@code ImageView}, {@code Label} for showing User the weapon on screen
      */
-    public void spinCase(double duration, double speed){
-        // IN PROGRESS
-        // for (ImageView image : imageList){
-        //     TranslateTransition transition = new TranslateTransition(new Duration(2000));
-        //     transition.setNode(image);
-        //     transition.setByX(190);
-        //     System.out.println(transition.getNode().getLayoutX());
-        //     if(image.getLayoutX() >= 1058){
-        //         try{
-        //             image.setImage(activeCase.getPrizeWeapon().getImage());
-        //         } catch (Exception exeption){
-        //             exeption.printStackTrace();
-        //         }
-        //         image.setLayoutX(68);
-        //         transition.setToX(0);
-        //     }
-        //     transition.play();
-        // }
+    public void spinCase(double duration, double speed, Pane pane, ImageView imageView, Label label){
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(speed), e -> {
             for (ImageView image : imageList){
                 image.setLayoutX(image.getLayoutX() + 2);
@@ -56,30 +48,55 @@ public class CaseSpinner {
         }));
         timeline.setCycleCount((40*(int)duration));
         timeline.play();
-        System.out.println(speed);
         timeline.setOnFinished(e -> {
             if(recursiveStopper < 30){
                 recursiveStopper++;
+                //When coming to and end set WonWeapon and slow speed
+                if(recursiveStopper >= 19){
+                    for (ImageView image : imageList){
+                        if(image.getLayoutX() > 68 && image.getLayoutX() < 268 && !hasChosenWeapon){
+                            getUserPrize();
+                            try {
+                                image.setImage(currentWonWeapon.getImage());
+                            } catch (IOException e1) {
+                                System.out.println("Holder meg til ett våpen");
+                            }
+                        }
+                    }
+                }
                 if(recursiveStopper >= 20){
-                    spinCase(duration- (duration/20), (speed*1.3));    
+                    spinCase(duration- (duration/20), (speed*1.3), pane, imageView, label);    
                 }else {
-                    spinCase(duration - (duration/10), (speed*1.1));
+                    spinCase(duration - (duration/10), (speed*1.1), pane, imageView, label);
                 }
             } else {
                 timeline.stop();
+                label.setText(currentWonWeapon.getName());
+                try {
+                    imageView.setImage(currentWonWeapon.getImage());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                pane.setVisible(true);
             }
-            //0.001 0.005 0.01 0.5
         });    
     }
 
     /**
      * Picks a random {@code Weapon} for the User to win
      * <p>
-     * Can be anything in the active Case's library
+     * Can be anything in the active Case's library but will only choose ONE weapon,
+     * <p>
+     * even when run more than ones in the same code
      */
     public Weapons getUserPrize(){
-        Weapons weaponWon = activeCase.getPrizeWeapon();
-        System.out.println(weaponWon.getName());
-        return weaponWon;
+        if(!hasChosenWeapon){
+            this.hasChosenWeapon = true;
+            Weapons weaponWon = activeCase.getPrizeWeapon();
+            System.out.println(weaponWon.getName());
+            this.currentWonWeapon = weaponWon;
+            return weaponWon;
+        }
+        return null;
     }
 }
