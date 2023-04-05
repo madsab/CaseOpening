40bfda27b834c34cf1bd92adb5842f89c2a3b5e8
madsab <mads.barnes@hotmail.com>
2023-04-05 18:41:15 +0200

    possible to make a user and check if input is good

--- src/main/java/caseOpening/fxui/CreateAccountController.java
+++ src/main/java/caseOpening/fxui/CreateAccountController.java
@@ -2,21 +2,37 @@ package caseOpening.fxui;
 
 import java.io.IOException;
 
+import caseOpening.logIn.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class CreateAccountController {
     @FXML private TextField createdUsername, createdPassword;
+    @FXML private Label createdUserResponse;
 
     @FXML
     public void createNewAccount(){
-        
+        try {
+            new User(createdUsername.getText(), createdPassword.getText());
+            createdUserResponse.setText("User created");
+            createdUserResponse.setStyle("-fx-text-fill: green;");
+        } catch (Exception e){
+            if(e.getClass().equals(IllegalArgumentException.class)){
+                createdUserResponse.setText("Username can't have special characters. \nPassword must have one number and one capital letter");
+                createdUserResponse.setStyle("-fx-text-fill: red;");
+            }
+            if(e.getClass().equals(IllegalStateException.class)){
+                createdUserResponse.setText("User allready exists");
+                createdUserResponse.setStyle("-fx-text-fill: red;");
+            }
+        }
     }
 
     @FXML
