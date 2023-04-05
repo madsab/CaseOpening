52b2af43852f90e1876cb907cadca6d30cf70e57
madsab <mads.barnes@hotmail.com>
2023-04-05 18:41:49 +0200

    check valid user on logIn

--- src/main/java/caseOpening/fxui/LogInController.java
+++ src/main/java/caseOpening/fxui/LogInController.java
@@ -2,26 +2,37 @@ package caseOpening.fxui;
 
 import java.io.IOException;
 
+import caseOpening.fileWriting.UserFileWriterReader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LogInController {
     @FXML private TextField username, password;
+    @FXML private Label logInResponse;
     
     //Takes you to homepage
     @FXML
     public void onLogIn(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("caseOpening/homePage.fxml"));
-        Stage stage = ((Stage)((Node)event.getSource()).getScene().getWindow());
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        UserFileWriterReader fr = new UserFileWriterReader();
+        if(fr.getUser(username.getText(), "src/main/resources/caseOpening/UserOverview.txt") == null){
+            logInResponse.setText("Can't find user");
+        } else if (!password.getText().equals(fr.getFromUser("password", username.getText(), "src/main/resources/caseOpening/UserOverview.txt"))){
+            logInResponse.setText("Not correct password");
+        }
+        else {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("caseOpening/homePage.fxml"));
+            Stage stage = ((Stage)((Node)event.getSource()).getScene().getWindow());
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
     //Takes you to create account page
