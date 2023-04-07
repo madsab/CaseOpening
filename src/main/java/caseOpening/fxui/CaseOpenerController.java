a1f2b7475b48204de6b6b61fbba6336467438b92
madsab <mads.barnes@hotmail.com>
2023-04-07 14:47:09 +0200

    setup popup when 0 keys

--- src/main/java/caseOpening/fxui/CaseOpenerController.java
+++ src/main/java/caseOpening/fxui/CaseOpenerController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import caseOpening.fileWriting.UserFileWriterReader;
 import caseOpening.openingCases.CaseRegular;
 import caseOpening.openingCases.CaseSpinner;
 import javafx.event.ActionEvent;
@@ -25,21 +26,35 @@ import javafx.stage.Stage;
  */
 public class CaseOpenerController implements Initializable {
     private static CaseRegular goldenCase = new CaseRegular();
-    @FXML private ImageView showCaseWeapon1, showCaseWeapon2, showCaseWeapon3, showCaseWeapon4, showCaseWeapon5, caseOpenedBackground, showWeaponWon, backIcon;
-    @FXML private Pane ShowWeaponPane;
-    @FXML private Label weaponName;
+    @FXML private ImageView showCaseWeapon1, showCaseWeapon2, showCaseWeapon3, showCaseWeapon4, showCaseWeapon5, caseOpenedBackground, showWeaponWon, backIcon, keyImageView;
+    @FXML private Pane ShowWeaponPane, noMoreKeysPane;
+    @FXML private Label weaponName, keyAmount;
     private CaseSpinner caseSpinner;
+    private UserFileWriterReader fw;
+    private String activeUsername;
+    private int activeUserkeys;
 
     @FXML
     public void spinCase(){
-        if(!caseSpinner.isSpinning()){
+        if(!(caseSpinner.isSpinning()) && activeUserkeys > 0){
+            activeUserkeys--;
+            fw.changeUser(activeUsername, "keys", String.valueOf(activeUserkeys), "src/main/resources/caseOpening/UserOverview.txt");
+            keyAmount.setText(": " + String.valueOf(activeUserkeys));
             caseSpinner.spinCase(10, 0.002, ShowWeaponPane, showWeaponWon, weaponName);
+        }
+        if(activeUserkeys == 0){
+            noMoreKeysPane.setVisible(true);
         }
     }
 
     @FXML
     public void hideShowWeapon(){
         ShowWeaponPane.setVisible(false);
+    }
+
+    @FXML
+    public void hideNoMoreKeys(){
+        noMoreKeysPane.setVisible(false);
     }
 
     @FXML
@@ -53,7 +68,10 @@ public class CaseOpenerController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //Set 5 random weapons on loading of scene
+        fw = new UserFileWriterReader();
+        activeUsername = fw.getUserNameFromLine(0, "src/main/resources/caseOpening/ActiveUser.txt");
+        activeUserkeys = Integer.parseInt(fw.getFromUser("keys", activeUsername, "src/main/resources/caseOpening/UserOverview.txt"));
+        //Set 5 random weapons on loading of scene and user key info
         try{
             caseOpenedBackground.setImage(new Image(new FileInputStream("./images/weapons-case-opened.png")));
             backIcon.setImage(new Image(new FileInputStream("./images/backIcon.png")));
@@ -62,6 +80,8 @@ public class CaseOpenerController implements Initializable {
             showCaseWeapon3.setImage(goldenCase.getPrizeWeapon().getImage());
             showCaseWeapon4.setImage(goldenCase.getPrizeWeapon().getImage());
             showCaseWeapon5.setImage(goldenCase.getPrizeWeapon().getImage());
+            keyImageView.setImage(new Image(new FileInputStream("./images/keyBlue.png")));
+            keyAmount.setText(": " + fw.getFromUser("keys", activeUsername, "src/main/resources/caseOpening/UserOverview.txt"));
             caseSpinner = new CaseSpinner(goldenCase,showCaseWeapon1,showCaseWeapon2,showCaseWeapon3,showCaseWeapon4, showCaseWeapon5);
         } catch (IOException e){
             e.printStackTrace();
