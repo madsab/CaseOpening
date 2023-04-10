08fbd27ee13875053671c016cb364ca8abcc2170
madsab <mads.barnes@hotmail.com>
2023-04-10 16:44:46 +0200

    new comparator for sorting by rarity

---
+++ src/main/java/caseOpening/tools/WeaponRarityComparator.java
@@ -0,0 +1,17 @@
+package caseOpening.tools;
+
+import java.util.Comparator;
+
+import caseOpening.weapons.Weapons;
+
+public class WeaponRarityComparator implements Comparator<Weapons> {
+
+    @Override
+    public int compare(Weapons o1, Weapons o2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compare'");
+    }
+
+    
+    
+}
