67d6bd720f5ec3e4f4f4c045cef4fc57eb5356b6
madsab <mads.barnes@hotmail.com>
2023-04-10 21:16:36 +0200

    completed

--- src/main/java/caseOpening/tools/WeaponRarityComparator.java
+++ src/main/java/caseOpening/tools/WeaponRarityComparator.java
@@ -8,8 +8,31 @@ public class WeaponRarityComparator implements Comparator<Weapons> {
 
     @Override
     public int compare(Weapons o1, Weapons o2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compare'");
+        int o1Value = rarityToValue(o1.getRarity());
+        int o2Value = rarityToValue(o2.getRarity());
+        if (o1Value > o2Value){
+            return -1;
+        } else if (o1Value == o2Value){
+            return 0;
+        } else {
+            return 1;
+        }
+
+    }
+    private int rarityToValue(String rarity){
+        switch(rarity){
+            case "legendary":
+                return 5;
+            case "epic":
+                return 4;
+            case "rare":
+                return 3;
+            case "uncommon":
+                return 2;
+            case "common":
+                return 1;
+        }
+        return 0;
     }
 
     
